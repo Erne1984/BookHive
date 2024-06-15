0247323fb45da38912f4cbc829ccf9f3da369579
Erne1984 <ernestofamorim@gmail.com>
2024-06-15 08:51:56 -0300

    testing functions

--- app/src/main/java/com/example/bookhive/MainActivity.kt
+++ app/src/main/java/com/example/bookhive/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.bookhive
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val input: EditText = findViewById(R.id.editTxt)
+        val btn: Button = findViewById(R.id.btn1)
+        val txt: TextView = findViewById(R.id.txtView)
+
+        btn.setOnClickListener{
+
+            val inputText = input.text.toString()
+
+            txt.text = inputText
+        }
+
     }
 }
