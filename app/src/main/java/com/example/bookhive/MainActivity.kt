463b24e4f23b4969256f6596c83eb34af55e7c80
Erne1984 <ernestofamorim@gmail.com>
2024-06-15 10:10:05 -0300

    ...

--- app/src/main/java/com/example/bookhive/MainActivity.kt
+++ app/src/main/java/com/example/bookhive/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.bookhive
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -15,11 +16,18 @@ class MainActivity : AppCompatActivity() {
         val btn: Button = findViewById(R.id.btn1)
         val txt: TextView = findViewById(R.id.txtView)
 
+        val btnNavigate: Button = findViewById(R.id.btnNavigate)
+
         btn.setOnClickListener{
 
             val inputText = input.text.toString()
 
             txt.text = inputText
+        }
+
+        btnNavigate.setOnClickListener {
+            val intent = Intent(this, activity_livros::class.java)
+            startActivity(intent)
         }
 
     }
---
+++ app/src/main/java/com/example/bookhive/activity_livros.kt
@@ -0,0 +1,11 @@
+package com.example.bookhive
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class activity_livros : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_livros)
+    }
+}
